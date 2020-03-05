d55205eafa016cf21888ffec486a57ea1d031635
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-03-05 12:51:18 -0500

    hooked up my app.js to the dom and created folders for my navbar and mobile navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(props) {
+  return (
+    <div className="App">
+      <h2>Welcome</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
