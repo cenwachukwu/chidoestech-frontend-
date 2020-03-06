3d9adcdd7136f6cb0b9c4238a9862da0e2cb2358
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-03-06 15:41:55 -0500

    using hooks for carousel

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -2,11 +2,16 @@ import React, { useState, useEffect, useMemo } from 'react';
 import { Link } from '@reach/router';
 
 const Carousel = props => {
+  const [isCurrentImageIndex, setIsCurrentImageIndex] = useState(0);
+
   return (
-    <div>
+    <div className="carousel">
       <h1>block</h1>
       {props.data.map((photo, index) => {
         console.log(photo.blogImage);
+        return (
+          <img key={index} src={photo.blogImage} alt="blog post thumbnail" />
+        );
       })}
     </div>
   );
